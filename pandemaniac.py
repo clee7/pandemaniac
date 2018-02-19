5966eb03a7429e42f730d5ef6601d0a213fbda12
Chih-Chieh Lee <clee7@caltech.edu>
2018-02-18 20:44:34 -0800

    change file path parsing

--- pandemaniac.py
+++ pandemaniac.py
@@ -10,21 +10,36 @@ def load_graph(filename):
     return graph
 
 
-def generate_random(nodes, num_seeds):
+def parse_file_path(file_path):
+    parts = path.rsplit('/', 1)
+    directory = ""
+    filename = parts[0]
+    if len(parts) >= 2:
+        directory = parts[0] + "/"
+        filename = parts[1]
+    nums = filename.split('.')
+    num_players = int(nums[0])
+    num_seeds = int(nums[1])
+    return (directory, filename, num_players, num_seeds)
+
+
+def generate_random(nodes, num_seeds, num_rounds):
+    # Get the list of nodes
+    nodes = list(graph.keys())
+
+    # Select random from nodes
     random_list = []
     np.random.shuffle(nodes)
     for i in range(num_seeds):
         random_list.append(nodes[i])
+    return random_list * num_rounds
 
-    return random_list
 
-
-def save_output(filename, strategies, num_rounds):
+def save_output(filename, strategies):
 
     file = open(filename, "w")
-    for i in range(num_rounds):
-        for j in range(len(strategies)):
-            file.writelines(str(strategies[j]) + '\n')
+    for j in range(len(strategies)):
+        file.writelines(str(strategies[j]) + '\n')
     return 0
 
 
@@ -33,22 +48,22 @@ if __name__ == "__main__":
         print("usage: python pandemaniac.py <path_to_json_file>")
         sys.exit(1)
 
-    filename = sys.argv[1]
+    path = sys.argv[1]
 
-    nums = filename.split('.')
-    num_players = int(nums[0])
-    num_seeds = int(nums[1])
+    # Define global var
     num_rounds = 50
 
-    # Get the adjacency list
-    graph = load_graph(filename)
+    # Parse the input path to find filename and number of players and seeds
+    (directory, filename, num_players, num_seeds) = parse_file_path(path)
 
-    # Get the list of nodes
-    nodes = list(graph.keys())
+    # Find output file name
+    output_filename = directory + filename.rsplit('.', 1)[0] + ".txt"
+
+    # Get the adjacency list
+    graph = load_graph(path)
 
     # Generate a list of random nodes as root nodes
-    strategies = generate_random(nodes, num_seeds)
+    strategy = generate_random(graph, num_seeds, num_rounds)
 
     # Save input file
-    output_filename = filename.rsplit('.', 1)[0] + ".txt"
-    save_output(output_filename, strategies, num_rounds)
+    save_output(output_filename, strategy)
